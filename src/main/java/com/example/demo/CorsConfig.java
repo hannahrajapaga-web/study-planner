55133ae58f1df588d6612e4334d756fb77944091
Hannah Rajapaga <hannahrajapaga@gmail.com>
2026-03-28 20:17:50 +0530

    Add CorsConfig for Netlify

---
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns(
+                                "http://localhost:3000",
+                                "https://sensational-tulumba-a36a3a.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
